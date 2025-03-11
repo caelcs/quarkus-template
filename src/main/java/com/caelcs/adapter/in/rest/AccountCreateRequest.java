d5ac5fae422f089d38eeeff806fa5b9727cbd6c5
Adolfo Secchi <adolfo.secchi@waitrose.co.uk>
2025-03-10 21:46:03 -0300

    removing all code and updating README

--- src/main/java/com/caelcs/adapter/in/rest/AccountCreateRequest.java
+++ src/main/java/com/caelcs/adapter/in/rest/AccountCreateRequest.java
@@ -7,7 +7,10 @@ import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 
 @Builder(toBuilder = true)
-public record AccountCreateRequest(@NotBlank(message="accountNumber may not be blank") String accountNumber, @NotNull(message="accountType may not be absent") AccountType accountType) {
+public record AccountCreateRequest(
+        @NotBlank(message="accountNumber may not be blank") String accountNumber,
+        @NotNull(message="accountType may not be absent") AccountType accountType
+) {
 
     AccountDTO toAccountDTO() {
         return AccountCreateRequestMapper.INSTANCE.toAccountDTOFrom(this);
