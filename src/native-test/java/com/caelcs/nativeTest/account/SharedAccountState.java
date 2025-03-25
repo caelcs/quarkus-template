4ce586c5e78701e6b99aea4642bc03484e74448a
Adolfo Custidiano <adolfoecs@gmail.com>
2025-03-25 08:53:58 -0300

    adding more specific features CI

--- src/native-test/java/com/caelcs/nativeTest/account/SharedAccountState.java
+++ src/native-test/java/com/caelcs/nativeTest/account/SharedAccountState.java
@@ -2,27 +2,16 @@ package com.caelcs.nativeTest.account;
 
 import com.caelcs.adapter.in.rest.account.AccountWebModel;
 import com.caelcs.application.dto.AccountDTO;
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+import lombok.Data;
 
 import java.util.List;
 
+@SuppressFBWarnings(value = {"EI2", "EI", "UwF", "NP"}, justification = "This list is safely managed elsewhere")
+@Data
 public class SharedAccountState {
 
     private List<AccountDTO> accounts;
     private List<AccountWebModel> response;
 
-    public List<AccountDTO> getAccounts() {
-        return accounts;
-    }
-
-    public void setAccounts(List<AccountDTO> accounts) {
-        this.accounts = accounts;
-    }
-
-    public List<AccountWebModel> getResponse() {
-        return response;
-    }
-
-    public void setResponse(List<AccountWebModel> response) {
-        this.response = response;
-    }
 }
