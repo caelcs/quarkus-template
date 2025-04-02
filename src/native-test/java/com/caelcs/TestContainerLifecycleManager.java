dee2018c8a354ce8f35c3a634c3e348a57859652
Adolfo Custidiano <adolfoecs@gmail.com>
2025-04-01 23:05:34 -0300

    inegration test starting keycloak and quarkus connecting with it

--- src/native-test/java/com/caelcs/TestContainerLifecycleManager.java
+++ src/native-test/java/com/caelcs/TestContainerLifecycleManager.java
@@ -1,9 +1,14 @@
 package com.caelcs;
 
+import dasniko.testcontainers.keycloak.KeycloakContainer;
 import io.quarkus.test.common.QuarkusTestResourceLifecycleManager;
 import org.testcontainers.containers.PostgreSQLContainer;
 
 import java.util.Map;
+
+import static io.restassured.RestAssured.given;
+import static java.util.concurrent.TimeUnit.SECONDS;
+import static org.testcontainers.shaded.org.awaitility.Awaitility.await;
 
 public class TestContainerLifecycleManager implements QuarkusTestResourceLifecycleManager {
 
@@ -12,13 +17,26 @@ public class TestContainerLifecycleManager implements QuarkusTestResourceLifecyc
             .withUsername("test")
             .withPassword("test");
 
+    public static final KeycloakContainer keycloakContainer = new KeycloakContainer("quay.io/keycloak/keycloak:26.1.4")
+            .withAdminUsername("admin")
+            .withAdminPassword("admin")
+            .withRealmImportFile("test-realm.json");
+
     @Override
     public Map<String, String> start() {
         postgreSQLContainer.start();
-        waitForDatabase();
-        return Map.of("quarkus.datasource.jdbc.url", postgreSQLContainer.getJdbcUrl(),
+        keycloakContainer.start();
+        waitForDatabaseIsReady();
+        waitForKeycloakIsReady(keycloakContainer.getAuthServerUrl());
+        return Map.of(
+                "quarkus.datasource.jdbc.url", postgreSQLContainer.getJdbcUrl(),
                 "quarkus.datasource.username", postgreSQLContainer.getUsername(),
-                "quarkus.datasource.password", postgreSQLContainer.getPassword());
+                "quarkus.datasource.password", postgreSQLContainer.getPassword(),
+                "keycloak.url", keycloakContainer.getAuthServerUrl(),
+                "keycloak.default.client-id", "quarkus-template-app",
+                "keycloak.default.client-secret", "thisisasecret",
+                "keycloak.default.realm", "quarkus-template"
+        );
     }
 
     @Override
@@ -26,7 +44,7 @@ public class TestContainerLifecycleManager implements QuarkusTestResourceLifecyc
         postgreSQLContainer.stop();
     }
 
-    private static void waitForDatabase() {
+    private static void waitForDatabaseIsReady() {
         try (var connection = postgreSQLContainer.createConnection("")) {
             try (var statement = connection.createStatement()) {
                 statement.execute("SELECT 1");
@@ -35,5 +53,20 @@ public class TestContainerLifecycleManager implements QuarkusTestResourceLifecyc
         } catch (Exception e) {
             throw new RuntimeException("Database did not become ready in time!", e);
         }
+    }
+
+    private void waitForKeycloakIsReady(String keycloakUrl) {
+        await().atMost(30, SECONDS).until(() -> {
+            int statusCode = given()
+                    .when().get(keycloakUrl + "/realms/quarkus-template")
+                    .then().extract().statusCode();
+
+            if (statusCode == 200) {
+                System.out.println("✅ Keycloak is ready!");
+                return true;
+            }
+
+            return false;
+        });
     }
 }
